b45cde35c61209d60ce06cdfc879b4b006c842d4
Jules Pénuchot <jules.penuchot@lri.fr>
2018-03-29 12:02:32 +0200

    Added base code for collision handling, modified constructors

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,44 +1,55 @@
 class Body
 {
-  constructor(mass, pos, friction, static = false)
+  constructor ( mass , pos
+              
+              , vel = new Vector(0, 0)
+              , static = false
+              )
   {
-    this.pos      = pos;
-    this.mass     = mass;
-    this.friction = friction;
-    this.static   = static;
+    this.pos        = pos;
+    this.vel        = vel;
+    this.mass       = mass;
+    this.static     = static;
   }
+
+  static elasticity = 1.;
 }
 
 class Sphere extends Body
 {
-  constructor(mass, pos, friction, rad)
+  constructor ( mass , pos
+              
+              , rad
+
+              , vel = new Vector(0, 0)
+              , static = false
+              )
   {
-    super(mass, pos, friction);
+    super(mass, pos, vel, elasticity);
     this.rad = rad;
   }
 }
 
 class Rectangle extends Body
 {
-  constructor(mass, pos, friction, dim)
+  constructor ( mass , pos
+              
+              , dim
+              
+              , vel = new Vector(0, 0)
+              , static = false
+              )
   {
-    super(mass, pos, friction);
+    super(mass, pos, vel, elasticity);
     this.dim = dim;
   }
 
-  minkowksiDiff(r) {
+  minkowksiDiff(r)
+  {
     let vec = this.pos.sub(r.pos).sub(dim);
-    return {vec , new Vector(this.dim.x + r.dim.x, this.dim.y + r.dim.y) };
-  }
-
-  hasOrigin() {
-    return  this.pos.x <= 0 
-        &&  this.pos.y <= 0
-        &&  this.pos.x + this.dim.x >= 0
-        &&  this.pos.y + this.dim.y >= 0;
+    return { pos: vec, dim: this.dim.add(r.dim); };
   }
 }
-
 
 class Physics
 {
@@ -51,16 +62,69 @@ class Physics
   }
   
   static collide_rs(r, s)
-  {
-    return null;  //  TEMP
-  }
+    { return {r, s} }
   
-  static collide_rr(r1, r2)
+  static collide_rr(ra, rb)
+    { return {ra, rb} }
+
+  static collide_rr (ra, rb)
   {
-      if (r1.minkowksiDiff(r2).hasOrigin()) return {r1, r2};
-      return null;  //  TEMP
-  }
+    let s = rb.minkowksiDiff(ra);
   
+    { //  Elasticity check
+
+    }
+
+    if (!ra.minkowksiDiff(rb).hasOrigin()) return {ra, rb};
+  
+    let up    = new Vector( 0                    , s.origin.y );
+    let down  = new Vector( 0                    , s.height + s.origin.y );
+    let left  = new Vector( s.origin.x           , 0 );
+    let right = new Vector( s.origin.x + s.width , 0 );
+    
+    let n;
+
+    let norm_u = up.norm();
+    let norm_d = down.norm();
+    let norm_l = left.norm();
+    let norm_r = right.norm();
+  
+    if(norm_u < norm_d && norm_u < norm_l && norm_u < norm_r) n = up;
+    else if (norm_d < norm_u && norm_d < norm_r && norm_d < norm_l) n = down;
+    else if (norm_l < norm_u && norm_l < norm_d && norm_l < norm_r) n = left;
+    else n = right;
+  
+    let nA = ra.velocity.norm() / ( ra.velocity.norm() + rb.velocity.norm() );
+    let nB = rb.velocity.norm() / ( ra.velocity.norm() + rb.velocity.norm() );
+  
+    if (ra.velocity.norm() == rb.velocity.norm())
+    {
+      if (ra.mass === Infinity && rb.mass === Infinity)
+        return null;
+      
+      if (ra.mass > rb.mass)
+        { nA = 0; nB = 1; }
+      else
+        { nA = 1; nB = 0; }
+    }
+  
+    //  MODIFY
+    //ra = ra.add(n.mult(nA));)
+    //  MODIFY
+    //rb = rb.add(n.mult(-nB));
+  
+    n = n.normalize();
+  
+    let rel_speed = ra.velocity.sub(rb.velocity);
+    
+    const e = Body.elasticity;
+  
+    let vC = n.mult(j * ra.invMass).add(ra.velocity);
+    let vB = rb.velocity.sub(n.mult(j * rb.invMass));
+  
+    return { velocity1 : vC , velocity2: vB };
+  }
+
   static collide(a, b) {
     return null;
 

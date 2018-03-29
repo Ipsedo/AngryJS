43282903c672bcbe4b45cf2dcd0fd4f722063eb9
samuel <berriens@gmail.com>
2018-03-29 22:05:39 +0200

    level loading via JSON works

--- scripts/game.js
+++ scripts/game.js
@@ -20,39 +20,12 @@ class Game
   start()
   {
     //  On amorce le jeu en appelant start
-      //TODO JSON pour level
-      let angle1 = Math.random() * Math.PI * 2;
-      let r1 = new Rectangle(1, new Vector(200, 200), Vector.fill(100),
-          new Vector(0.5 * Math.cos(angle1), 0.5 * Math.sin(angle1)));
-      let s1 = new RectSprite(this.context, r1, [255, 0, 0]);
-      this.entities.push(new Entity(r1, s1, 10));
 
-      let angle2 = Math.random() * Math.PI * 2;
-      let r2 = new Rectangle(1, new Vector(500, 200), Vector.fill(100),
-          new Vector(0.5 * Math.cos(angle2), 0.5 * Math.sin(angle2)));
-      let s2 = new RectSprite(this.context, r2, [255, 0, 0]);
-      this.entities.push(new Entity(r2, s2, 100));
-
-      let wall_up_body = new Rectangle(Infinity, new Vector(0,-10), new Vector(this.windowW, 20));
-      let wall_down_body = new Rectangle(Infinity, new Vector(0, this.windowH - 10), new Vector(this.windowW, 20));
-      let wall_left_body = new Rectangle(Infinity, new Vector(-10, 10), new Vector(20, this.windowH - 20));
-      let wall_right_body = new Rectangle(Infinity, new Vector(this.windowW - 10, 10), new Vector(20, this.windowH - 10));
-
-      let wall_up_sprite = new RectSprite(this.context, wall_up_body, [0, 0, 0]);
-      let wall_down_sprite = new RectSprite(this.context, wall_down_body, [0, 0, 0]);
-      let wall_left_sprite = new RectSprite(this.context, wall_left_body, [0, 0, 0]);
-      let wall_rigth_sprite = new RectSprite(this.context, wall_right_body, [0, 0, 0]);
-
-      this.entities.push(new Entity(wall_up_body, wall_up_sprite, Infinity));
-      this.entities.push(new Entity(wall_down_body, wall_down_sprite, Infinity));
-      this.entities.push(new Entity(wall_left_body, wall_left_sprite, Infinity));
-      this.entities.push(new Entity(wall_right_body, wall_rigth_sprite, Infinity));
-
-      /*let levelLoader = new LevelLoader(this.context);
+      let levelLoader = new LevelLoader(this.context);
       let that = this;
-      levelLoader.load("../res/level.json", (e) => {
+      levelLoader.load("./res/level0.json", (e) => {
           that.entities = e;
-      });*/
+      });
 
       requestAnimationFrame(this.update.bind(this));
   }

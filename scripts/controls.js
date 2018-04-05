6a8e6ed515f27eb1153440f108601988cac5bf27
Ipsedo <7268@samu>
2018-04-05 15:54:05 +0200

    puissance tir ajustable + sprite

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,42 +1,88 @@
 class Controls {
-  constructor(canvas, onFire, maxVelLaunch) {
+
+  static get LaunchCoeff () { return 1e-2; }
+  static get MaxLaunchNorm () { return 2; }
+
+  constructor(canvas, context, onFire) {
     this.canvas = canvas;
+
+    this.sprite = new Arrow(context, [255, 255, 255]);
 
     /**
      * Call back pour le tir de missile : function(pos, vel)
      */
     this.onFire = onFire;
 
-    this.maxVelLaunch = maxVelLaunch;
+    this.startingDrag = false;
+
+    this.canvas.addEventListener("mouseout", () => this.startingDrag = false);
 
     this.fstPos = Vector.fill(0.);
-
-    let that = this;
-
-    /**
-     * On récupère la position intitiale
-     * et on passe à vrai la variable indiquant le début du drag and drop
-     */
-    this.canvas.onmousedown = function (e) {
-      that.fstPos.x = e.clientX;
-      that.fstPos.y = e.clientY;
-    };
+    this.launchVec = Vector.fill(0.);
 
     /**
-     * Si il y a eu drag and drop,
-     * on récupère les vecteur de fin de drag,
-     * on calcul le vecteur resultant du début du drag à la fin
-     * On normalise ce vecteur avec une norme limite
+     * Mouse callBack
      */
-    this.canvas.onmouseup = function (e) {
-      let finalPos = new Vector(e.clientX, e.clientY);
-      let launchVec = that.fstPos.sub(finalPos);
-      let norm = launchVec.norm();
-      norm = norm < that.maxVelLaunch ? norm : that.maxVelLaunch;
-      launchVec = launchVec.normalize().mul(norm);
-      if (launchVec.norm() > 0)
-        that.onFire(that.fstPos, launchVec);
+    this.canvas.onmousedown = this.draggingStart.bind(this);
+    this.canvas.onmouseup = this.draggingEnd.bind(this);
+    this.canvas.onmousemove = this.dragging.bind(this);
 
-    };
+    /**
+     * Touch callBack
+     */
+    this.canvas.addEventListener("touchstart", this.draggingStart.bind(this));
+    this.canvas.addEventListener("touchend", this.draggingEnd.bind(this));
+    this.canvas.addEventListener("touchmove", this.dragging.bind(this));
+  }
+
+  dragging(e) {
+    // Dernière position du pointeur
+    let finalPos = new Vector(this.canvas.width * e.clientX / this.canvas.clientWidth,
+      this.canvas.height * e.clientY / this.canvas.clientHeight);
+
+    // Vecteur de la dernière position à la première
+    let launchVec = this.fstPos.sub(finalPos);
+
+    // On multiplie la norme de ce vecteur par un coeff pour attenuer
+    let norm = launchVec.norm() * Controls.LaunchCoeff;
+
+    // On limite cette norme
+    norm = norm < Controls.MaxLaunchNorm ? norm : Controls.MaxLaunchNorm;
+
+    // Le vecteur de tir final est normalisé puis multiplié par la norme calculée avant
+    launchVec = launchVec.normalize().mul(norm);
+
+    // Assignation vecteur pour le dessin du control
+    this.launchVec = launchVec;
+  }
+
+  /**
+   * On récupère la position intitiale
+   * et on passe à vrai la variable indiquant le début du drag and drop
+   */
+  draggingStart(e) {
+    this.startingDrag = true;
+    this.fstPos.x = this.canvas.width * e.clientX / this.canvas.clientWidth;
+    this.fstPos.y = this.canvas.height * e.clientY / this.canvas.clientHeight;
+    this.launchVec = Vector.fill(0.)
+  }
+
+  /**
+   * Si il y a eu drag and drop,
+   * on récupère les vecteur de fin de drag,
+   * on calcul le vecteur resultant du début du drag à la fin
+   * On normalise ce vecteur avec une norme limite
+   */
+  draggingEnd(e) {
+    if (this.launchVec.norm() > 0) {
+      this.onFire(this.fstPos, this.launchVec);
+    }
+    this.startingDrag = false;
+  }
+
+  draw() {
+    if(this.startingDrag) {
+      this.sprite.draw(this.fstPos, this.launchVec.div(Controls.LaunchCoeff));
+    }
   }
 }

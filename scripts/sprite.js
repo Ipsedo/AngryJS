47084cbb23e65436a683ea9bbb66636c073c2be0
samuel <berriens@gmail.com>
2018-04-28 02:51:15 +0200

    add Frames sprite class

--- scripts/sprite.js
+++ scripts/sprite.js
@@ -60,6 +60,79 @@ class ImageRectSprite extends Sprite {
   }
 }
 
+class FramesRectSprite extends Sprite {
+
+  constructor(context, rect, imageURIs, explosionColor) {
+    super(context, explosionColor);
+    this.rect = rect;
+    let that = this;
+
+    let prom = new Promise(function (resolve) {
+      let sprites = [];
+      let img = new Image();
+      img.onload = () => {
+        sprites.push(img);
+        resolve(sprites);
+      };
+      img.src = imageURIs.shift();
+    });
+
+    imageURIs.forEach((uri) =>
+      prom = prom.then(function (fulfilled) {
+        return new Promise(function (resolve) {
+          let img = new Image();
+          img.onload = () => {
+            fulfilled.push(img);
+            return resolve(fulfilled);
+          };
+          img.src = uri;
+        });
+      })
+    );
+
+    prom.then(function (fulfilled) {
+      that.sprites = fulfilled;
+      that.nbFrame = fulfilled.length;
+      that.isReady = true;
+    });
+
+    this.cpt = 0;
+    this.maxFPS = 60;
+  }
+
+  draw() {
+    if (this.isReady) {
+      let currImg = Math.trunc(this.nbFrame * this.cpt / this.maxFPS);
+      this.context.drawImage(this.sprites[currImg],
+        this.rect.pos.x, this.rect.pos.y,
+        this.rect.dim.x, this.rect.dim.y);
+      this.cpt = ++this.cpt % this.maxFPS;
+    }
+  }
+
+  static Bird(context, rect, index) {
+    let path = "./res/bird_" + index + "/frame-";
+    let URIs = [];
+    for (let i = 1; i <= 8; i++) {
+      URIs.push(path + i + ".png");
+    }
+    return new FramesRectSprite(context, rect, URIs, [0, 0, 225]);
+
+  }
+
+  static Bird_1(context, rect) {
+    return FramesRectSprite.Bird(context, rect, 1);
+  }
+
+  static Bird_2(context, rect) {
+    return FramesRectSprite.Bird(context, rect, 2);
+  }
+
+  static Bird_3(context, rect) {
+    return FramesRectSprite.Bird(context, rect, 3);
+  }
+}
+
 class Point extends Sprite {
   constructor(context, particule, color) {
     super(context, color);
